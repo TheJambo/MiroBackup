18a44a4b81aeb619c088a23eed3223b7b197cf8a
James Osborn <james@james.am>
2021-07-19 22:57:52 +0100

    Update MoveToDaily.ps1

--- MoveToDaily.ps1
+++ MoveToDaily.ps1
@@ -2,6 +2,6 @@ $Sourcefolder= "C:\MiroBackup\5Min\"
 $Targetfolder= "C:\MiroBackup\Daily Backup\"
 Get-ChildItem -Path $Sourcefolder -Recurse|
 Where-Object {
-  $_.LastWriteTime -gt [datetime]::Now.AddMinutes(-40)
+  $_.LastWriteTime -gt [datetime]::Now.AddMinutes(-5)
 }| Copy-Item -Destination $Targetfolder
 Get-ChildItem -Path $Sourcefolder -Include *.rtb -File -Recurse | foreach { $_.Delete()}
